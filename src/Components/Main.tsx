b38ee0cf2aba6332c240401430408a11566d7e95
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-09-22 13:24:24 +0200

    feat: reading file names in tabs

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,93 +1,23 @@
 import { Box, Grid, List, ListItemButton, ListItemText, Tabs, Tab, Typography, Divider } from "@mui/material";
 import { useEffect, useState } from "react";
 import CodeEditor from "./CodeEditor";
-import { styled } from '@mui/material/styles';
-
-interface TabPanelProps {
-  children?: React.ReactNode;
-  index: number;
-  value: number;
-}
-
-function CustomTabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-interface StyledTabsProps {
-  children?: React.ReactNode;
-  value: number;
-  onChange: (event: React.SyntheticEvent, newValue: number) => void;
-}
-
-const StyledTabs = styled((props: StyledTabsProps) => (
-  <Tabs
-    {...props}
-    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
-  />
-))({
-  '& .MuiTabs-indicator': {
-    display: 'flex',
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
-  },
-  '& .MuiTabs-indicatorSpan': {
-    maxWidth: 40,
-    width: '100%',
-    backgroundColor: '#635ee7',
-  },
-});
-
-interface StyledTabProps {
-  label: string;
-}
-
-const StyledTab = styled((props: StyledTabProps) => (
-  <Tab disableRipple {...props} />
-))(({ theme }) => ({
-  textTransform: 'none',
-  fontWeight: theme.typography.fontWeightRegular,
-  fontSize: theme.typography.pxToRem(15),
-  marginRight: theme.spacing(1),
-  color: 'rgba(255, 255, 255, 0.7)',
-  '&.Mui-selected': {
-    color: '#fff',
-  },
-  '&.Mui-focusVisible': {
-    backgroundColor: 'rgba(100, 95, 228, 0.32)',
-  },
-}));
-
+import StyledTabs from "./StyledTabs";
+import StyledTab from "./StyledTab";
+import CustomTabPanel from "./CustomTabPanel";
 
 const Main = () => {
   const [selectedIndex, setSelectedIndex] = useState(1);
-  const files: { [key: string]: { name: string; value: string } } = {
-    'singleton.txt': {
-      name: 'singleton.txt',
-      value: "singletonecode"
+  const files: { [key: string]: {files: Array<string> } } = {
+    'singleton': {
+      files:["singleton.txt"]
     },
-    'builder.txt': {
-      name: 'builder.txt',
-      value: "buildecode"
+    'builder': {
+      files:["builder.txt", "director.txt"]
     },
   };
-  const [fileName, setFileName] = useState("singleton.txt");
-  const file = files[fileName];
+
+  const [choosenDesignPatternName, setChoosenDesignPatternName] = useState("singleton");
+  const choosenDesignPattern = files[choosenDesignPatternName];
   const [fileContent, setFileContent] = useState("");
   const [value, setValue] = useState(0);
 
@@ -96,9 +26,9 @@ const Main = () => {
   };
 
 
-  const handleFileRead = async () => {
+  const handleFileRead = async (filename: string) => {
     try {
-      const response = await fetch(fileName);
+      const response = await fetch(filename);
       console.log(response);
       if (!response.ok) {
         throw new Error('Nie udało się pobrać pliku.');
@@ -115,9 +45,9 @@ const Main = () => {
 
 
   useEffect(() => {
-    handleFileRead();
+    handleFileRead(choosenDesignPattern.files[0]);
 
-  }, [fileName])
+  }, [choosenDesignPattern])
 
 
   // const handleListItemClick = (
@@ -130,8 +60,8 @@ const Main = () => {
   const handleListItemClick = (index: number) => {
 
     switch (index) {
-      case 1: setFileName("singleton.txt"); break;
-      case 2: setFileName("builder.txt"); break;
+      case 1: setChoosenDesignPatternName("singleton"); break;
+      case 2: setChoosenDesignPatternName("builder"); break;
 
     }
     setSelectedIndex(index);
@@ -177,22 +107,27 @@ const Main = () => {
               onChange={handleChange}
               aria-label="styled tabs example"
             >
-              <StyledTab label="Workflows" />
+              {choosenDesignPattern.files.map(tabName => {
+                return(
+                  <StyledTab label={tabName} />
+                );
+              })}
+              {/* <StyledTab label="Workflows" />
               <StyledTab label="Datasets" />
-              <StyledTab label="Connections" />
+              <StyledTab label="Connections" /> */}
             </StyledTabs>
 
             <Divider />
 
             <CustomTabPanel value={value} index={0}>
-              <CodeEditor path={file.name} content={fileContent} />
+              <CodeEditor path={choosenDesignPattern.files[0]} content={fileContent} />
             </CustomTabPanel>
-            <CustomTabPanel value={value} index={1}>
-              <CodeEditor path={file.name} content={fileContent} />
+            {/* <CustomTabPanel value={value} index={1}>
+              <CodeEditor path={file.name} content={"ujauja"} />
             </CustomTabPanel>
             <CustomTabPanel value={value} index={2}>
-              <CodeEditor path={file.name} content={fileContent} />
-            </CustomTabPanel>
+              <CodeEditor path={file.name} content={"hehehe"} />
+            </CustomTabPanel> */}
           </Box>
 
         </Grid>
---
+++ src/Components/StyledTabs.tsx
@@ -0,0 +1,29 @@
+import { Tabs } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+interface StyledTabsProps {
+    children?: React.ReactNode;
+    value: number;
+    onChange: (event: React.SyntheticEvent, newValue: number) => void;
+}
+
+const StyledTabs = styled((props: StyledTabsProps) => (
+    <Tabs
+    {...props}
+    TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+  />
+  ))({
+    '& .MuiTabs-indicator': {
+        display: 'flex',
+        justifyContent: 'center',
+        backgroundColor: 'transparent',
+    },
+    '& .MuiTabs-indicatorSpan': {
+        maxWidth: 40,
+        width: '100%',
+        backgroundColor: '#635ee7',
+    },
+});
+
+export default StyledTabs;
+
