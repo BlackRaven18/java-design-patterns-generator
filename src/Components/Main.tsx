42c2b23efbc34ad75dac871e287c591acdad006c
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-09-24 20:11:36 +0200

    fix: reading value from file by monaco editor

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -3,7 +3,8 @@ import { ReactComponentElement, ReactElement, useEffect, useState } from "react"
 import CodeEditor from "./CodeEditor";
 
 const Main = () => {
-  const [selectedIndex, setSelectedIndex] = useState(1);
+  const [selectedPatternIndex, setSelectedPatternIndex] = useState(1);
+
   const files: { [key: string]: { files: Array<string> } } = {
     'singleton': {
       files: ["singleton.txt"],
@@ -13,50 +14,40 @@ const Main = () => {
     },
   };
 
-  const [choosenDesignPatternName, setChoosenDesignPatternName] = useState("singleton");
-
-  let choosenDesignPattern = files[choosenDesignPatternName];
-  const [currentSelectedTab, setCurrentSelectedTab] = useState(1);
-  //let tmpEditor: React.ReactNode[] = [];
-  const [tmpEditor, setTmpEditor] = useState<React.ReactNode[]>([]);
-
-  const updateChoosenDesignPattern = () => {
-    choosenDesignPattern = files[choosenDesignPatternName];
-  }
-
-  const updateEditorTabs = () => {
-    //setTmpEditor([<CodeEditor path={choosenDesignPattern.files[0]} pattern={choosenDesignPatternName} />]);
-    tmpEditor.length = 0;
-    choosenDesignPattern.files.map((fileName, index) => {
-      tmpEditor.push(<CodeEditor key={index} path={fileName} pattern={choosenDesignPatternName} />)
-    });
-  }
+  const [selectedPattern, setSelectedPattern] = useState(files['singleton']);
+  const [editorTabs, setEditorTabs] = useState<React.ReactNode[]>([]);
 
   useEffect(() => {
-    updateEditorTabs();
+    loadEditorTabs();
+  }, [selectedPatternIndex])
 
-  }, [choosenDesignPatternName])
+  const loadEditorTabs = () => {
+    editorTabs.length = 0;
+    //setEditorTabs([]);
 
+    let tabs: React.ReactNode[] = [];
 
-  const handleListItemClick = (index: number) => {
+    selectedPattern.files.map((pathToFile, index) => {
+      tabs.push(<CodeEditor key={index} pathToSourceFile={pathToFile} />)
+    })
 
-    switch (index) {
-      case 1: setChoosenDesignPatternName("singleton"); break;
-      case 2: setChoosenDesignPatternName("builder"); break;
-    }
+    setEditorTabs(tabs);
 
-    // updateChoosenDesignPattern();
-
-    setSelectedIndex(index);
-    setCurrentSelectedTab(1);
-
-  };
-
-  const handleTabClick = (index: number) => {
-    setCurrentSelectedTab(index);
   }
 
 
+  const handlePatternChange = (patternIndex: number) => {
+
+    switch (patternIndex) {
+      case 1: setSelectedPattern(files['singleton']); break;
+      case 2: setSelectedPattern(files['builder']); break;
+      default: break;
+    }
+
+
+    setSelectedPatternIndex(patternIndex);
+
+  }
 
   return (
     <Box
@@ -73,14 +64,15 @@ const Main = () => {
         <Grid item xs={2}>
           {<List component="nav">
             <ListItemButton
-              selected={selectedIndex === 1}
-              onClick={() => handleListItemClick(1)}
+              selected={selectedPatternIndex === 1}
+              onClick={() => handlePatternChange(1)}
             >
               <ListItemText primary="Singleton" />
+
             </ListItemButton>
             <ListItemButton
-              selected={selectedIndex === 2}
-              onClick={() => handleListItemClick(2)}
+              selected={selectedPatternIndex === 2}
+              onClick={() => handlePatternChange(2)}
             >
               <ListItemText primary="Builder" />
             </ListItemButton>
@@ -94,33 +86,15 @@ const Main = () => {
           }}>
 
             <List component={Stack} direction="row">
-              {choosenDesignPattern.files.map((fileName, index) => {
-                return (
-                  <ListItemButton
-                    key={index}
-                    selected={currentSelectedTab === index}
-                    onClick={() => handleTabClick(index)}
-                  >
-
-                    <ListItemText primary={fileName} />
-                  </ListItemButton>
-                );
-              })}
 
             </List>
-            { tmpEditor[currentSelectedTab]}
-            {/* {tmpEditor.map(currentEditorTab => {
-              return(
-                currentEditorTab
-              );
-            })} */}
-            {/* //{tmpEditor[0]} */}
-
-
-
-
 
           </Box>
+          {editorTabs.map(editorTab => {
+            return (
+              editorTab
+            );
+          })}
 
 
 
