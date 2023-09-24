8d8d4dd39498f3d99b4487253af966eb9518080c
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-09-24 20:11:36 +0200

    fix: reading value from file by monaco editor

--- src/Components/CodeEditor.tsx
+++ src/Components/CodeEditor.tsx
@@ -3,48 +3,62 @@ import { Editor } from "@monaco-editor/react";
 import { Box } from "@mui/material";
 
 interface CodeEditorProps {
-    path: string,
-    pattern: string,
+    pathToSourceFile: string,
 }
 
 
 const CodeEditor: React.FC<CodeEditorProps> = (props: CodeEditorProps) => {
 
     const [content, setContent] = useState("");
+    const [initialized, isInitialized] = useState(false);
 
     const handleFileRead = async (filename: string) => {
 
         let content = "";
         try {
-          const response = await fetch(filename);
-          if (!response.ok) {
-            throw new Error('Nie udało się pobrać pliku.');
-          }
-    
-          content = await response.text();
-    
+            const response = await fetch(filename);
+            if (!response.ok) {
+                throw new Error('Nie udało się pobrać pliku.');
+            }
+
+            content = await response.text();
+
         } catch (error) {
-          console.error('Błąd podczas pobierania pliku:', error);
+            console.error('Błąd podczas pobierania pliku:', error);
         } finally {
-          return content;
+            return content;
         }
-      }
-
-    useEffect(() => {
-        setEditorContent();
-        console.log(props.pattern);
-
-    }, [props.pattern])
-
-    const setEditorContent = () => {
-        handleFileRead(props.path).then(fileContent => {
-            setContent(fileContent);
-        })
     }
 
-    function handleEditorChange(value: string | undefined) {
-        value? setContent(value) : setContent("");
+    useEffect(() => {
+
+        //if (!initialized) {
+            handleFileRead(props.pathToSourceFile).then(fileContent => {
+                setContent(fileContent);
+                isInitialized(true);
+            })
+        //}
+
         
+
+    }, [props.pathToSourceFile])
+
+    // useEffect(() => {
+    //     setEditorContent();
+    //     console.log(props.pattern);
+
+    // }, [props.pattern])
+
+    // const setEditorContent = () => {
+    //     handleFileRead(props.path).then(fileContent => {
+    //         setContent(fileContent);
+    //     })
+    // }
+
+    function handleEditorChange(value: string | undefined) {
+        value ? setContent(value) : setContent("");
+
+
     }
 
 
@@ -59,7 +73,7 @@ const CodeEditor: React.FC<CodeEditorProps> = (props: CodeEditorProps) => {
                 height="90vh"
                 width="80vw"
                 defaultLanguage="java"
-                path={props.path}
+                path={props.pathToSourceFile}
                 value={content}
                 theme='vs-dark'
                 options={{
