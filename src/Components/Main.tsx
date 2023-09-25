fa3f6a27fab6a59f19a1faf534b803a6847a94be
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-09-25 21:58:45 +0200

    refactor: monaco editor behaviour

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,51 +1,83 @@
+import { Editor } from "@monaco-editor/react";
 import { Box, Grid, List, ListItemButton, ListItemText, Stack } from "@mui/material";
-import { ReactComponentElement, ReactElement, useEffect, useState } from "react";
-import CodeEditor from "./CodeEditor";
+import { useEffect, useState } from "react";
 
 const Main = () => {
-  const [selectedPatternIndex, setSelectedPatternIndex] = useState(1);
+  const [selectedPatternIndex, setSelectedPatternIndex] = useState(0);
+  const [selectedTabIndex, setSelectedTabIndex] = useState(0);
 
-  const files: { [key: string]: { files: Array<string> } } = {
+  const files: { [key: string]: { filesNames: string[] } } = {
     'singleton': {
-      files: ["singleton.txt"],
+      filesNames: ["singleton.txt"],
     },
     'builder': {
-      files: ["builder.txt", "director.txt"],
+      filesNames: ["builder.txt", "director.txt"],
     },
   };
 
+
   const [selectedPattern, setSelectedPattern] = useState(files['singleton']);
-  const [editorTabs, setEditorTabs] = useState<React.ReactNode[]>([]);
+  const [editorLoadedFileName, setEditorLoadedFileName] = useState('singleton.txt');
+  const [tmpEditorContent, setTmpEditorContent] = useState('bbb');
+
 
   useEffect(() => {
-    loadEditorTabs();
-  }, [selectedPatternIndex])
-
-  const loadEditorTabs = () => {
-    editorTabs.length = 0;
-    //setEditorTabs([]);
-
-    let tabs: React.ReactNode[] = [];
-
-    selectedPattern.files.map((pathToFile, index) => {
-      tabs.push(<CodeEditor key={index} pathToSourceFile={pathToFile} />)
-    })
-
-    setEditorTabs(tabs);
-
-  }
+    getFileContent(handleFileRead(editorLoadedFileName));
+  }, [])
 
 
-  const handlePatternChange = (patternIndex: number) => {
+  const handlePatternChange = (patternIndex: number) =>  {
 
+    let selectedPattern: string = "";
     switch (patternIndex) {
-      case 1: setSelectedPattern(files['singleton']); break;
-      case 2: setSelectedPattern(files['builder']); break;
+      case 0: selectedPattern = 'singleton'; break;
+      case 1: selectedPattern = 'builder'; break;
       default: break;
     }
 
+    handleFileRead(files[selectedPattern].filesNames[0]).then(fileContent => {
+      setTmpEditorContent(fileContent);
+      setSelectedPatternIndex(patternIndex);
+      setSelectedPattern(files[selectedPattern]);
+      setEditorLoadedFileName(files[selectedPattern].filesNames[0]);
+    })
 
-    setSelectedPatternIndex(patternIndex);
+  }
+
+  const handleTabChange = (index: number) => {
+      
+    handleFileRead(selectedPattern.filesNames[index]).then(fileContent => {
+      setTmpEditorContent(fileContent);
+      setSelectedTabIndex(index);
+      setEditorLoadedFileName(selectedPattern.filesNames[index]);
+    })
+
+  }
+
+  const handleFileRead = async (filename: string) => {
+
+    let content = "";
+    try {
+      const response = await fetch(filename);
+      if (!response.ok) {
+        throw new Error('Nie udało się pobrać pliku.');
+      }
+
+      content = await response.text();
+
+    } catch (error) {
+      console.error('Błąd podczas pobierania pliku:', error);
+    } finally {
+      return content;
+    }
+  }
+
+  const getFileContent = (fileReadResponse: Promise<string>) => {
+
+    fileReadResponse.then(content => {
+      setTmpEditorContent(content);
+
+    })
 
   }
 
@@ -64,15 +96,15 @@ const Main = () => {
         <Grid item xs={2}>
           {<List component="nav">
             <ListItemButton
-              selected={selectedPatternIndex === 1}
-              onClick={() => handlePatternChange(1)}
+              selected={selectedPatternIndex === 0}
+              onClick={() => handlePatternChange(0)}
             >
               <ListItemText primary="Singleton" />
 
             </ListItemButton>
             <ListItemButton
-              selected={selectedPatternIndex === 2}
-              onClick={() => handlePatternChange(2)}
+              selected={selectedPatternIndex === 1}
+              onClick={() => handlePatternChange(1)}
             >
               <ListItemText primary="Builder" />
             </ListItemButton>
@@ -86,15 +118,30 @@ const Main = () => {
           }}>
 
             <List component={Stack} direction="row">
+              {selectedPattern.filesNames.map((fileName, index) => {
+                return (
+                  <ListItemButton
+                    key={index}
+                    selected={selectedTabIndex === index}
+                    onClick={() => handleTabChange(index)}
+                  >
+
+                    <ListItemText primary={fileName} />
+                  </ListItemButton>
+                );
+              })}
 
             </List>
 
           </Box>
-          {editorTabs.map(editorTab => {
-            return (
-              editorTab
-            );
-          })}
+          <Editor
+            height="80vh"
+            theme="vs-dark"
+            path={editorLoadedFileName}
+            defaultLanguage={"java"}
+            defaultValue={tmpEditorContent}
+          />
+
 
 
 
