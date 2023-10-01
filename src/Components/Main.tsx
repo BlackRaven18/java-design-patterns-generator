41cfe928d63b7a0f0c85340d8183bd4178a1e7a3
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-10-01 11:14:00 +0200

    reafactor: pattern list

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,6 +1,8 @@
 import { Editor } from "@monaco-editor/react";
-import { Box, Grid, List, ListItemButton, ListItemText, Stack } from "@mui/material";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { Box, Collapse, Grid, List, ListItemButton, ListItemText, Stack } from "@mui/material";
 import { useEffect, useState } from "react";
+import AppConfigData from "./../app_config.json";
 
 interface Config {
   patternFamillies: PatternFamillyInfo[]
@@ -14,109 +16,84 @@ interface PatternFamillyInfo {
 
 interface PatternInfo {
   patternName: string,
-  filesName: string[]
+  patternFilesDirectory: string,
+  fileNames: string[]
 }
 
 
 const Main = () => {
 
-  const filesTmp: Config = {
-    patternFamillies: [
-      {
-        patternFamillyName: "Creational",
-        patterns: [
-          {
-            patternName: "Singleton",
-            filesName: ["singleton.txt"]
-          },
-          {
-            patternName: "Builder",
-            filesName: ["builder.txt", "director.txt"]
-          }
-        ]
-      },
-      {
-        patternFamillyName: "Structural",
-        patterns: [
-          {
-            patternName: "S1",
-            filesName: []
-          },
-          {
-            patternName: "S2",
-            filesName: []
-          }
-        ]
-      },
-      {
-        patternFamillyName: "Behavioral",
-        patterns: [
-          {
-            patternName: "B1",
-            filesName: []
-          },
-          {
-            patternName: "B2",
-            filesName: []
-          }
-        ]
-      },
 
-    ]
-  }
+  const [appConfig, setAppConfig] = useState<Config>({
+    patternFamillies: []
+  });
 
-  const files: { [key: string]: { filesNames: string[] } } = {
-    'singleton': {
-      filesNames: ["singleton.txt"],
-    },
-    'builder': {
-      filesNames: ["builder.txt", "director.txt"],
-    },
-  };
-
-
+  const [selectedPatternFamillyIndex, setSelectedPatternFamillyIndex] = useState(0);
   const [selectedPatternIndex, setSelectedPatternIndex] = useState(0);
   const [selectedTabIndex, setSelectedTabIndex] = useState(0);
+  const [open, setOpen] = useState(true);
 
-  const [selectedPattern, setSelectedPattern] = useState(files['singleton']);
+  const [selectedPattern, setSelectedPattern] = useState<PatternInfo>({
+    patternName: "",
+    patternFilesDirectory: "",
+    fileNames: []
+  })
+  //const [selectedPattern, setSelectedPattern] = useState(files['singleton']);
   const [editorLoadedFileName, setEditorLoadedFileName] = useState('singleton.txt');
   const [tmpEditorContent, setTmpEditorContent] = useState('bbb');
 
 
   useEffect(() => {
+
     getFileContent(handleFileRead(editorLoadedFileName));
+
+    // handleFileRead("D:/Semestr 7/prototyp pracy 3 react ts/my-app/public/app_config.json").then(content => {
+
+    let tmp: Config = JSON.parse(JSON.stringify(AppConfigData));
+    setSelectedPattern(tmp.patternFamillies[0].patterns[0]);
+    setAppConfig(tmp);
+    //});
+
   }, [])
 
 
-  const handlePatternChange = (patternIndex: number) => {
 
-    let selectedPattern: string = "";
-    switch (patternIndex) {
-      case 0: selectedPattern = 'singleton'; break;
-      case 1: selectedPattern = 'builder'; break;
-      default: break;
-    }
+  const handlePatterFamillyChange = (patternFamilly: PatternFamillyInfo, index: number) => {
+    setSelectedPatternFamillyIndex(index);
+    //setSelectedPattern(patternFamilly.patterns[0])
+    handlePatternChange(patternFamilly.patterns[0], 0);
+    setOpen(!open);
+  }
 
-    handleFileRead(files[selectedPattern].filesNames[0]).then(fileContent => {
+  const handlePatternChange = (pattern: PatternInfo, index: number) => {
+
+
+    handleFileRead(pattern.patternFilesDirectory + "/" + pattern.fileNames[0]).then(fileContent => {
       setTmpEditorContent(fileContent);
-      setSelectedPatternIndex(patternIndex);
-      setSelectedPattern(files[selectedPattern]);
-      setEditorLoadedFileName(files[selectedPattern].filesNames[0]);
+      //setSelectedPatternIndex(patternIndex);
+      //setSelectedPattern(files[selectedPattern]);
+      setSelectedPatternIndex(index);
+      setSelectedTabIndex(0);
+      setSelectedPattern(pattern);
+      setEditorLoadedFileName(pattern.fileNames[0]);
     })
 
   }
 
   const handleTabChange = (index: number) => {
 
-    handleFileRead(selectedPattern.filesNames[index]).then(fileContent => {
+    handleFileRead(selectedPattern.fileNames[index]).then(fileContent => {
       setTmpEditorContent(fileContent);
       setSelectedTabIndex(index);
-      setEditorLoadedFileName(selectedPattern.filesNames[index]);
+      setEditorLoadedFileName(selectedPattern.fileNames[index]);
     })
 
   }
 
+
   const handleFileRead = async (filename: string) => {
+
+    //console.log(fs.readFileSync("tmp.txt"))
 
     let content = "";
     try {
@@ -126,6 +103,11 @@ const Main = () => {
       }
 
       content = await response.text();
+
+      if (content.includes("<!DOCTYPE html>")) {
+        content = "//No files found...";
+        throw new Error("File is missing!");
+      }
 
     } catch (error) {
       console.error('Błąd podczas pobierania pliku:', error);
@@ -157,19 +139,55 @@ const Main = () => {
       >
         <Grid item xs={2}>
           {<List component="nav">
-            <ListItemButton
-              selected={selectedPatternIndex === 0}
-              onClick={() => handlePatternChange(0)}
-            >
-              <ListItemText primary="Singleton" />
 
-            </ListItemButton>
-            <ListItemButton
-              selected={selectedPatternIndex === 1}
-              onClick={() => handlePatternChange(1)}
-            >
-              <ListItemText primary="Builder" />
-            </ListItemButton>
+            {appConfig.patternFamillies.map((patternFamilly, index) => {
+              return (
+                <Box key={index}>
+                  <ListItemButton
+                    sx={{
+                      backgroundColor: "#2ECC71",
+                      '&.Mui-selected': {
+                        backgroundColor: '#58D68D',
+                      },
+                    }}
+                    key={index}
+                    selected={selectedPatternFamillyIndex === index}
+                    onClick={() => handlePatterFamillyChange(patternFamilly, index)}
+                  >
+                    <ListItemText primary={patternFamilly.patternFamillyName} />
+                    {selectedPatternFamillyIndex === index ? <ExpandLess /> : <ExpandMore />}
+
+                  </ListItemButton>
+
+
+                  <Collapse in={selectedPatternFamillyIndex === index} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                      {patternFamilly.patterns.map((pattern, index) => {
+                        return (
+                          <ListItemButton
+                            key={index}
+                            sx={{ 
+                              pl: 4,
+                              '&.Mui-selected': {
+                                backgroundColor: '#82E0AA ',
+                              },
+                             }}
+                            selected={selectedPatternIndex === index}
+                            onClick={() => handlePatternChange(pattern, index)}
+                          >
+                            <ListItemText primary={pattern.patternName} />
+                          </ListItemButton>
+                        );
+                      })}
+
+                    </List>
+                  </Collapse>
+
+                </Box>
+
+              );
+            })}
+
           </List>}
 
         </Grid>
@@ -180,7 +198,7 @@ const Main = () => {
           }}>
 
             <List component={Stack} direction="row">
-              {selectedPattern.filesNames.map((fileName, index) => {
+              {selectedPattern.fileNames.map((fileName, index) => {
                 return (
                   <ListItemButton
                     key={index}
