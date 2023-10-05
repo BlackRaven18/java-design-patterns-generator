236e04c89d6feb44b490ea8f0e4aee307924b8ff
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-10-05 10:58:24 +0200

    refactor: moved patterns menu to separate file

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,10 +1,8 @@
 import { Editor } from "@monaco-editor/react";
-import { ExpandLess, ExpandMore } from "@mui/icons-material";
-import { Box, Button, Collapse, Grid, List, ListItemButton, ListItemText, Stack, TextField } from "@mui/material";
+import { Box, Grid, List, ListItemButton, ListItemText, Stack, TextField } from "@mui/material";
 import { useEffect, useRef, useState } from "react";
-import { Config, LoadedPatternFileInfo, PatternFamillyInfo, PatternInfo } from "../types";
+import { LoadedPatternFileInfo, PatternFamillyInfo, PatternInfo } from "../types";
 import CustomBackdrop from "./CustomBackdrop";
-import DownloadButton from "./DownloadButton";
 
 
 import { Monaco } from "@monaco-editor/react";
@@ -18,6 +16,8 @@ import {
   setSelectedTabIndex
 } from "../redux/AppStateSlice";
 import { RootState } from "../redux/store";
+import FileReader from "../utils/FileReader";
+import PatternsMenu from "./PatternsMenu";
 
 
 const Main = () => {
@@ -26,10 +26,6 @@ const Main = () => {
 
   const editorRef = useRef<editor.IStandaloneCodeEditor | null>(null);
 
-  const appConfig = useSelector((state: RootState) => state.appState.appConfig);
-
-  const selectedPatternFamillyIndex = useSelector((state: RootState) => state.appState.selectedPatternFamillyIndex);
-  const selectedPatternIndex = useSelector((state: RootState) => state.appState.selectedPatternIndex);
   const selectedTabIndex = useSelector((state: RootState) => state.appState.selectedTabIndex);
 
   const [isLoading, setIsLoading] = useState(false);
@@ -43,8 +39,7 @@ const Main = () => {
   useEffect(() => {
 
     //let loadedPatternFile: LoadedPatternFileInfo = JSON.parse(JSON.stringify(selectedFile));
-
-    handleFileRead(selectedPattern.patternFilesDirectory + "/" + selectedFile.name)
+    FileReader.handleFileRead(selectedPattern.patternFilesDirectory + "/" + selectedFile.name)
       .then(fileContent => {
         let loadedPatternFile: LoadedPatternFileInfo = {
           name: selectedFile.name,
@@ -70,7 +65,7 @@ const Main = () => {
   const loadEditorValueArray = () => {
     let tmpArray = new Array<string>(selectedPattern.files.length);
     selectedPattern.files.map((file, index) => {
-      handleFileRead(selectedPattern.patternFilesDirectory + "/" + file.name)
+      FileReader.handleFileRead(selectedPattern.patternFilesDirectory + "/" + file.name)
         .then(fileContent => {
           tmpArray[index] = fileContent;
         })
@@ -79,11 +74,6 @@ const Main = () => {
     setEditorValueArray(tmpArray);
   }
 
-  const handlePatterFamillyChange = (patternFamilly: PatternFamillyInfo, index: number) => {
-
-    dispatch(setSelectedPatternFamillyIndex(index));
-    handlePatternChange(patternFamilly.patterns[0], 0);
-  }
 
   const handlePatternChange = (pattern: PatternInfo, index: number) => {
 
@@ -92,7 +82,7 @@ const Main = () => {
     dispatch(setSelectedPattern(pattern));
     dispatch(setSelectedPatternIndex(index));
 
-    handleFileRead(pattern.patternFilesDirectory + "/" + pattern.files[0].name)
+    FileReader.handleFileRead(pattern.patternFilesDirectory + "/" + pattern.files[0].name)
       .then(fileContent => {
         let newLoadedFile: LoadedPatternFileInfo = {
           name: pattern.files[0].name,
@@ -116,7 +106,7 @@ const Main = () => {
   const handleTabChange = (index: number) => {
 
 
-    handleFileRead(selectedPattern.patternFilesDirectory + "/" + selectedPattern.files[index].name)
+    FileReader.handleFileRead(selectedPattern.patternFilesDirectory + "/" + selectedPattern.files[index].name)
       .then(fileContent => {
         let newLoadedFile: LoadedPatternFileInfo = {
           name: selectedPattern.files[index].name,
@@ -131,29 +121,6 @@ const Main = () => {
   }
 
 
-  const handleFileRead = async (filename: string) => {
-
-    let content = "";
-    try {
-      const response = await fetch(filename);
-      if (!response.ok) {
-        throw new Error('Can not fetch a file');
-      }
-
-      content = await response.text();
-
-      if (content.includes("<!DOCTYPE html>")) {
-        content = "//No files found...";
-        throw new Error("File is missing!");
-      }
-
-    } catch (error) {
-      console.error('Could not read a file:', error);
-    } finally {
-      return content;
-    }
-  }
-
 
   const handleEditorChange = (value: string) => {
 
@@ -165,16 +132,10 @@ const Main = () => {
     editorRef.current = editor;
   }
 
-  const getEditorValue = () => {
-    alert(editorRef?.current?.getValue() ?? "nothing")
-  }
 
   const handleParamsChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     let tmp: string = selectedFile.content.replace("$CLASSNAME$", event.target.value);
     editorRef?.current?.setValue(tmp);
-
-
-
   }
 
   return (
@@ -201,60 +162,7 @@ const Main = () => {
             minWidth: 150
           }}
         >
-          <List component="nav">
-
-            {appConfig.patternFamillies.map((patternFamilly, index) => {
-              return (
-                <Box key={index}>
-                  <ListItemButton
-                    sx={{
-                      backgroundColor: "#2ECC71",
-                      '&.Mui-selected': {
-                        backgroundColor: '#58D68D',
-                      },
-                    }}
-                    key={index}
-                    selected={selectedPatternFamillyIndex === index}
-                    onClick={() => handlePatterFamillyChange(patternFamilly, index)}
-                  >
-                    <ListItemText primary={patternFamilly.patternFamillyName} />
-                    {selectedPatternFamillyIndex === index ? <ExpandLess /> : <ExpandMore />}
-
-                  </ListItemButton>
-
-
-                  <Collapse in={selectedPatternFamillyIndex === index} timeout="auto" unmountOnExit>
-                    <List component="div" disablePadding>
-                      {patternFamilly.patterns.map((pattern, index) => {
-                        return (
-                          <ListItemButton
-                            key={index}
-                            sx={{
-                              pl: 4,
-                              '&.Mui-selected': {
-                                backgroundColor: '#82E0AA ',
-                              },
-                            }}
-                            selected={selectedPatternIndex === index}
-                            onClick={() => handlePatternChange(pattern, index)}
-                          >
-                            <ListItemText primary={pattern.patternName} />
-                          </ListItemButton>
-                        );
-                      })}
-
-                    </List>
-                  </Collapse>
-
-                </Box>
-
-              );
-            })}
-
-          </List>
-          <DownloadButton editorValueArray={editorValueArray} selectedPattern={selectedPattern} />
-          <Button onClick={() => getEditorValue()}>Show editor content</Button>
-
+          <PatternsMenu />
         </Grid>
         <Grid
           item
--- src/Components/PatternsMenu.tsx
+++ src/Components/PatternsMenu.tsx
@@ -1,99 +1,115 @@
-// import { ExpandLess, ExpandMore } from "@mui/icons-material";
-// import { Box, Collapse, List, ListItemButton, ListItemText } from "@mui/material";
-// import { useDispatch, useSelector } from "react-redux";
-// import { RootState } from "../redux/store";
-// import { PatternInfo } from "../types";
-// import FileReader from "../utils/FileReader";
-// import {
-//    setEditorLoadedFileName,
-//    setSelectedPattern,
-//    setSelectedPatternIndex,
-//    setSelectedTabIndex
-// } from "../redux/AppStateSlice";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+import { Box, Collapse, List, ListItemButton, ListItemText } from "@mui/material";
+import { useDispatch, useSelector } from "react-redux";
+import {
+   setSelectedFile,
+   setSelectedPattern,
+   setSelectedPatternFamillyIndex,
+   setSelectedPatternIndex,
+   setSelectedTabIndex
+} from "../redux/AppStateSlice";
+import { AppDispatch, RootState } from "../redux/store";
+import { LoadedPatternFileInfo, PatternFamillyInfo, PatternInfo } from "../types";
+import FileReader from "../utils/FileReader";
 
 const PatternsMenu = () => {
 
-   // const dispatch = useDispatch();
+   const dispatch = useDispatch<AppDispatch>();
 
-   // const appConfig = useSelector((state: RootState) => state.appState.appConfig);
-   // const selectedPatternFamillyIndex = useSelector((state: RootState) => state.appState.selectedPatternFamillyIndex);
-   // const selectedPatternIndex = useSelector((state: RootState) => state.appState.selectedPatternIndex);
+   const appConfig = useSelector((state: RootState) => state.appState.appConfig);
+   const selectedPatternFamillyIndex = useSelector((state: RootState) => state.appState.selectedPatternFamillyIndex);
+   const selectedPatternIndex = useSelector((state: RootState) => state.appState.selectedPatternIndex);
 
-   // const handlePatternChange = (pattern: PatternInfo, index: number) => {
+   const handlePatterFamillyChange = (patternFamilly: PatternFamillyInfo, index: number) => {
 
-   //    //setIsLoading(true);
-   //    FileReader.handleFileRead(pattern.patternFilesDirectory + "/" + pattern.files[0]).then(fileContent => {
-   //       dispatch(setSelectedPattern(pattern));
-   //       setTmpEditorContent(fileContent);
+      dispatch(setSelectedPatternFamillyIndex(index));
+      handlePatternChange(patternFamilly.patterns[0], 0);
+   }
 
-   //       dispatch(setSelectedPatternIndex(index));
-   //       dispatch(setSelectedTabIndex(0));
+   const handlePatternChange = (pattern: PatternInfo, index: number) => {
 
-   //       setEditorLoadedFileName(pattern.files[0].name);
-   //       setEditorValueArray(new Array<string>(pattern.files.length))
+      //setIsLoading(true);
 
-   //       //setIsLoading(false);
-   //    })
+      dispatch(setSelectedPattern(pattern));
+      dispatch(setSelectedPatternIndex(index));
 
+      FileReader.handleFileRead(pattern.patternFilesDirectory + "/" + pattern.files[0].name)
+         .then(fileContent => {
+            let newLoadedFile: LoadedPatternFileInfo = {
+               name: pattern.files[0].name,
+               loaded: pattern.files[0].loaded,
+               content: fileContent,
+            }
 
-   //    return (
-   //       <Box>
-   //          <List component="nav">
-
-   //             {appConfig.patternFamillies.map((patternFamilly, index) => {
-   //                return (
-   //                   <Box key={index}>
-   //                      <ListItemButton
-   //                         sx={{
-   //                            backgroundColor: "#2ECC71",
-   //                            '&.Mui-selected': {
-   //                               backgroundColor: '#58D68D',
-   //                            },
-   //                         }}
-   //                         key={index}
-   //                         selected={selectedPatternFamillyIndex === index}
-   //                         onClick={() => handlePatterFamillyChange(patternFamilly, index)}
-   //                      >
-   //                         <ListItemText primary={patternFamilly.patternFamillyName} />
-   //                         {selectedPatternFamillyIndex === index ? <ExpandLess /> : <ExpandMore />}
-
-   //                      </ListItemButton>
+            dispatch(setSelectedFile(newLoadedFile));
+            dispatch(setSelectedTabIndex(0));
+         })
 
 
-   //                      <Collapse in={selectedPatternFamillyIndex === index} timeout="auto" unmountOnExit>
-   //                         <List component="div" disablePadding>
-   //                            {patternFamilly.patterns.map((pattern, index) => {
-   //                               return (
-   //                                  <ListItemButton
-   //                                     key={index}
-   //                                     sx={{
-   //                                        pl: 4,
-   //                                        '&.Mui-selected': {
-   //                                           backgroundColor: '#82E0AA ',
-   //                                        },
-   //                                     }}
-   //                                     selected={selectedPatternIndex === index}
-   //                                     onClick={() => handlePatternChange(pattern, index)}
-   //                                  >
-   //                                     <ListItemText primary={pattern.patternName} />
-   //                                  </ListItemButton>
-   //                               );
-   //                            })}
+      //setEditorValueArray(new Array<string>(pattern.files.length))
 
-   //                         </List>
-   //                      </Collapse>
+      //setIsLoading(false);
 
-   //                   </Box>
+   }
 
-   //                );
-   //             })}
+   return (
+      <Box>
+         <List component="nav">
 
-   //          </List>
-   //          {/* <DownloadButton editorValueArray={editorValueArray} selectedPattern={selectedPattern} />
-   //          <Button onClick={() => getEditorValue()}>Show editor content</Button> */}
-   //       </Box>
-   //    );
-   // }
+            {appConfig.patternFamillies.map((patternFamilly, index) => {
+               return (
+                  <Box key={index}>
+                     <ListItemButton
+                        sx={{
+                           backgroundColor: "#2ECC71",
+                           '&.Mui-selected': {
+                              backgroundColor: '#58D68D',
+                           },
+                        }}
+                        key={index}
+                        selected={selectedPatternFamillyIndex === index}
+                        onClick={() => handlePatterFamillyChange(patternFamilly, index)}
+                     >
+                        <ListItemText primary={patternFamilly.patternFamillyName} />
+                        {selectedPatternFamillyIndex === index ? <ExpandLess /> : <ExpandMore />}
+
+                     </ListItemButton>
+
+
+                     <Collapse in={selectedPatternFamillyIndex === index} timeout="auto" unmountOnExit>
+                        <List component="div" disablePadding>
+                           {patternFamilly.patterns.map((pattern, index) => {
+                              return (
+                                 <ListItemButton
+                                    key={index}
+                                    sx={{
+                                       pl: 4,
+                                       '&.Mui-selected': {
+                                          backgroundColor: '#82E0AA ',
+                                       },
+                                    }}
+                                    selected={selectedPatternIndex === index}
+                                    onClick={() => handlePatternChange(pattern, index)}
+                                 >
+                                    <ListItemText primary={pattern.patternName} />
+                                 </ListItemButton>
+                              );
+                           })}
+
+                        </List>
+                     </Collapse>
+
+                  </Box>
+
+               );
+            })}
+
+         </List>
+         {/* <DownloadButton editorValueArray={editorValueArray} selectedPattern={selectedPattern} />
+            <Button onClick={() => getEditorValue()}>Show editor content</Button> */}
+      </Box>
+   );
 }
+
 
 export default PatternsMenu;
