4234cbde7a93adec7e3d5adcb9c8b078f650c3b4
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-11-07 22:04:52 +0100

    refactor: DownloadButton

--- src/Components/TopBar.tsx
+++ src/Components/TopBar.tsx
@@ -1,10 +1,11 @@
-import { Box, IconButton, Toolbar, Typography } from "@mui/material";
+import { Box, Divider, IconButton, Stack, Toolbar, Typography } from "@mui/material";
 import { useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../redux/store";
 import MenuIcon from '@mui/icons-material/Menu';
 import { useDispatch } from "react-redux";
 import { setIsDrawerOpen } from "../redux/AppStateSlice";
 import EditorReadOnlyContainer from "./ReadOnlySwitch/EditorReadOnlyContainer";
+import DownloadButton from "./DownloadButton";
 
 export default function TopBar() {
 
@@ -41,7 +42,22 @@ export default function TopBar() {
                     {selectedPattern.name}
                 </Typography>
 
+                <Stack
+                    direction={"row"}
+                    spacing={"10px"}
+                >
                 <EditorReadOnlyContainer />
+                <Divider
+                    orientation="vertical"
+                    variant="middle"
+                    sx={{
+                        backgroundColor: "secondary.main"
+                    }}
+                    flexItem
+                />
+                <DownloadButton selectedPattern={selectedPattern} />
+
+                </Stack>
             </Toolbar>
 
         </Box>
