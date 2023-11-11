81be551c1a683da5b7f9e9a3401506630dc780d8
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-11-11 19:17:49 +0100

    feat: Object Pool pattern files

---
+++ public/others/Object_pool/PooledObject.java
@@ -0,0 +1,12 @@
+public class PooledObject {
+
+    public String data;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
