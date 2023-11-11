8dd770ac5fa8faf02390f942269135c696fceb34
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-11-11 17:19:59 +0100

    feat: parameters in Composite pattern

--- public/structural_patterns/Composite/Component.java
+++ public/structural_patterns/Composite/Component.java
@@ -1,6 +1,6 @@
-public interface Component {
-    void operation();
-    void add(Component component);
-    void remove(Component component);
-    Component getChild(int childIndex);
+public abstract class  $COMPONENT_CLASS_NAME$ {
+    void $OPERATION_METHOD_NAME$(){}
+    void $ADD_METHOD_NAME$($COMPONENT_CLASS_NAME$ component){}
+    void $REMOVE_METHOD_NAME$($COMPONENT_CLASS_NAME$ component){}
+    $COMPONENT_CLASS_NAME$ $GETCHILD_METHOD_NAME$(int childIndex){ return null;}
 }
