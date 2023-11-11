745ba40a92add9ff59189a6e5f494cb071c5513e
Arkadiusz Wolski <arkadiusz.wolski4@gmail.com>
2023-11-11 13:10:09 +0100

    feat: pattern files for: Interpreter, Strategy, Template Method, Visitor

---
+++ public/behavioral_patterns/Template_method/AbstractClass.java
@@ -0,0 +1,15 @@
+public abstract class AbstractClass {
+
+    public void templateMethod(){
+        primitiveOperation1();
+        primitiveOperation2();
+    }
+
+    public void primitiveOperation1(){
+
+    }
+
+    public void primitiveOperation2(){
+
+    }
+}
---
+++ public/behavioral_patterns/Template_method/ConcreteClass.java
@@ -0,0 +1,12 @@
+public class ConcreteClass extends AbstractClass{
+
+    @Override
+    public void primitiveOperation1() {
+        super.primitiveOperation1();
+    }
+
+    @Override
+    public void primitiveOperation2() {
+        super.primitiveOperation2();
+    }
+}
